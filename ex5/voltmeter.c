fff3101a5c6a9dde9d204c6a44c24514a42b7378
Ramy <ramses11.rea@gmail.com>
2018-04-20 14:04:55 +0100

    updated voltmeter

--- ex5/voltmeter.c
+++ ex5/voltmeter.c
@@ -15,15 +15,17 @@
 // General variable setup
 	// Setting up flags
 	int holdFlag = 0;	// 1 if hold, switch using the interrupt
+	int adcFlag = 0;	// Decides which ADC to use (0 for ADC1, 1 for ADC2)
 	// To add flags for switching ADC later on	
 
 	// Setting storage variable for raw ADC output
 	unsigned int adc1 = 0;
-	//unsigned int adc2 = 0;
+	unsigned int adc2 = 0;
 
 	// Setting storage variable for actual voltage
+	float voltage = 0;
 	float voltage1 = 0;
-	//float voltage2 = 0;
+	float voltage2  0;
 
 void interrupt isr(){
 	// Reset interrupt flag
@@ -53,13 +55,19 @@ void welcome(){
 	__delay_ms(1500);
 }
 
-int voltage(){
+int voltage(int adcFlag){
 	// Measuring voltage from ADC
 	// Returns actual voltage
 	// Maybe it checks which ADC is enabled and returns relevant voltage
 	// 10-bit ADC so 1024 discrete voltage points between 0.25V and Vdd
-	adc1 = readADC();
-	voltage1 = (adc1*4.75)/1023;
+	switch (a){
+		case 1:
+			adc1 = readADC();
+			voltage1 = (adc1*4.75)/1023;
+		case 2:
+			adc2 = readADC();
+			voltage2 = (adc2*4.75)/1023;
+	}
 }
 
 int hold(){
@@ -113,8 +121,13 @@ void main(){
 				// Read and display ADC voltage
 				// Print voltage1
 				// Delay
+				voltage = voltage();
+				Lcd_Clear();
+				Lcd_Write_Char(voltage);
 			case 1:
 				// Hold last measured ADC voltage
+				Lcd_Clear();
+				Lcd_Write_Char(voltage);
 		}
 	}
 }
